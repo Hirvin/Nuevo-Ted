5f4ce8907d1d78d03752ec970c73d2e28fde02ba
Hirvin <luishirvin@gmail.com>
2018-08-12 17:50:30 -0500

    versio xml handler completa

--- XmlHandler.py
+++ XmlHandler.py
@@ -46,8 +46,8 @@ class FrameTime(object):
         self.time_milis += self.hour * 60 * 60 * 1000
 
     def __str__(self):
-        txt = "%d:%d:%d.%d => %d" % (self.hour, self.min,
-                                     self.sec, self.mil, self.time_milis)
+        txt = "%d:%d:%d.%d" % (self.hour, self.min,
+                               self.sec, self.mil)
         return txt
 
 
@@ -59,17 +59,23 @@ class SbvToXml(object):
         self.start_time = FrameTime()
         self.end_time = FrameTime()
         self.data_buffer = []
+        self.xml = FrameXml()
+        self.xml.create_head(self.xml.root)
 
     def open_sbv(self, path):
         """ abre y porcesa el archivo sbv """
         with open(path, 'r') as f_read:
             read_data = f_read.read()
             self.in_buffer = read_data.replace("\r", "")
-            self.in_buffer = self.in_buffer.replace("[br]", "")
+            self.in_buffer = self.in_buffer.replace("[br]", " ")
             self.in_buffer = self.in_buffer.replace("\n", " ").split(" ")
             f_read.close()
             return True
         return False
+
+    def close(self, path):
+        """ close all elements """
+        self.xml.save(path)
 
     def print_buffer(self):
         """ print buffer """
@@ -78,6 +84,9 @@ class SbvToXml(object):
 
     def get_frames(self):
         """ get frames """
+        frame_index = 1
+        l1_data = None
+        l2_data = None
         while self.in_buffer != []:
 
             # line = self.in_buffer.pop(0)
@@ -85,13 +94,21 @@ class SbvToXml(object):
             print "Start Frame ##################"
             self.get_start_time()
             if self.get_data_frame() is False:
-                return False
+                break
+            l1_data = self.data_buffer
             print "---------------------------"
             self.get_end_time()
             if self.get_data_frame() is False:
-                return False
+                break
+            l2_data = self.data_buffer
+            self.xml.create_frame(self.xml.root, frame_number=frame_index,
+                                  init_time=self.start_time,
+                                  end_time=self.end_time, l1_data=l1_data,
+                                  l2_data=l2_data)
             print "End Frame ####################\n"
-            return True
+            frame_index += 1
+        self.xml.set_head(n_frames=frame_index - 1)
+        return True
 
     def get_start_time(self):
         """ get start time """
@@ -136,7 +153,7 @@ class SbvToXml(object):
             print word
             self.data_buffer.append(word)
             word = self.in_buffer.pop(0)
-        if len(self.data_buffer > NUM_WORD_BY_SUB):
+        if len(self.data_buffer) > NUM_WORD_BY_SUB:
             print "data buffes have more than 15 elements"
             return False
         return True
@@ -161,45 +178,66 @@ class FrameXml(object):
         head_title = ET.SubElement(head, "Title")
         head_number_frames = ET.SubElement(head, "Number_frames")
         head_number_completed_frames = ET.SubElement(head, "Number_completed_frames")
-        head_title.text = "hola Mundo"
+        head_title.text = " "
         head_number_frames.text = "0"
         head_number_completed_frames.text = "0"
+        self.head = head
         return head
 
-    def create_frame(self, parent):
+    def set_head(self, title="X", n_frames=1, n_completed=1):
+        """ set head fields """
+        self.head[0].text = title
+        self.head[1].text = str(n_frames)
+        self.head[2].text = str(n_completed)
+
+    def create_frame(self, parent, frame_number=1, init_time=None, end_time=None, l1_data=None, l2_data=None):
         """ creat one single frame """
-        frame = self.init_frame(parent)
+        frame = self.init_frame(parent, frame_number, init_time=init_time, end_time=end_time, l1_data=l1_data, l2_data=l2_data)
         return frame
 
-    def init_frame(self, parent):
+    def init_frame(self, parent, frame_number=1, init_time=None, end_time=None, l1_data=None, l2_data=None):
         """ create Frame """
         frame = ET.SubElement(parent, "Frame")
-        frame.set('number', '1')
+        frame.set('number', str(frame_number))
 
         frame_init_time = ET.SubElement(frame, "init_time")
+        frame_init_time_utf = ET.SubElement(frame, "init_time_utf")
+        if init_time is not None:
+            frame_init_time.text = str(init_time.get_time_milis())
+            frame_init_time_utf.text = str(init_time)
+
         frame_end_time = ET.SubElement(frame, "end_time")
+        frame_end_time_utf = ET.SubElement(frame, "end_time_utf")
+        if end_time is not None:
+            frame_end_time.text = str(end_time.get_time_milis())
+            frame_end_time_utf.text = str(end_time)
+
         text_line1 = ET.SubElement(frame, "text_line1")
         text_line2 = ET.SubElement(frame, "text_line2")
-        self.create_text_line(text_line1, "text_line1")
-        self.create_text_line(text_line2, "text_line2")
 
-        frame_init_time.text = "0"
-        frame_end_time.text = "0"
+        if l1_data is not None:
+            self.create_text_line(text_line1, "text_line1", data=l1_data)
+        if l2_data is not None:
+            self.create_text_line(text_line2, "text_line2", data=l2_data)
 
         return frame
 
-    def create_text_line(self, parent, name):
+    def create_text_line(self, parent, name, data=None):
         """ create text line """
         text_line = ET.SubElement(parent, name)
-        self.create_words(text_line)
+        self.create_words(text_line, data=data)
         return text_line
 
-    def create_words(self, parent):
+    def create_words(self, parent, data=None):
         """ create all words """
         lst_words = []
+        text = " "
         for index in range(NUM_WORD_BY_SUB):
             word = self.init_word(parent)
-            self.set_word(word, index, " ")
+            if index < len(data):
+                text = data[index]
+            self.set_word(word, index, text)
+            text = " "
             lst_words.append(word)
 
     def init_word(self, parent):
@@ -228,10 +266,41 @@ class FrameXml(object):
         tree.write("test.xml", xml_declaration=True, encoding='utf-8')
 
 
-xml = FrameXml()
-xml.create()
-xml.save("test.xml")
 
-# svb = SbvToXml()
-# svb.open_sbv("test.sbv")
-# svb.get_frames()
+
+class HandlerXml(object):
+    """ hand all xml files """
+    def __init__(self):
+        tree = ET.parse("test.xml")
+        self.root = tree.getroot()
+        self.head_index = 0
+        self.frame_index = 1
+        self.frame_len = len(self.root) - 1
+
+    def next_frame(self):
+        """ return the next frame """
+        if self.frame_index < self.frame_len:
+            self.frame_index += 1
+        return self.root[self.frame_index]
+
+    def prev_frame(self):
+        """ return prev frame """
+        if self.frame_index > 1:
+            self.frame_index -= 1
+        return self.root[self.frame_index]
+
+    def get_frame(self):
+        """ return actal frame """
+        return self.root[self.frame_index]
+
+
+
+def sbv_to_xml(sbv_file=None, out_file=None):
+    svb = SbvToXml()
+    svb.open_sbv("test.sbv")
+    svb.get_frames()
+    svb.close("test.xml")
+
+
+# CREAR EL ARCHIVO XML A PARTIR DE UN SBV
+# sbv_to_xml(sbv_file="test.sbv", out_file="test.xml")
